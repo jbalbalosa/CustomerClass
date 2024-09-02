ad20b4db649730a9fe2c6eb09f88061b76582a03
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-02 15:15:54 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+             Customer customerBob = new Customer("Bob",2000,"bob@email.com");
+        System.out.println(customerBob.getCustomerName());
+        System.out.println(customerBob.getCreditLimit());
+        System.out.println(customerBob.getEmailAddress());
+
+        Customer customerJeff = new Customer();
+        System.out.println(customerJeff.getCustomerName());
+        System.out.println(customerJeff.getCreditLimit());
+        System.out.println(customerJeff.getEmailAddress());
+
+        Customer thirdCustomer = new Customer("joe","joe@email.com");
+        System.out.println(thirdCustomer.getCustomerName());
+        System.out.println(thirdCustomer.getCreditLimit());
+        System.out.println(thirdCustomer.getEmailAddress());
     }
 }
