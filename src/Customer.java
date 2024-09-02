7ff8a49a0e311161405c5c8dcce2f3964530644f
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-02 15:15:27 +0900

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,40 @@
 package PACKAGE_NAME;
 
 public class Customer {
+    private String customerName;
+    private double creditLimit;
+    private  String emailAddress;
+
+    //No args Constructor
+    public Customer(){
+//        this("default name",500,"default email");
+//        System.out.println("Empty constructor called");
+
+        this("default name","default email");
+
+    }
+
+    //Second constructor
+    public Customer(String customerName, String emailAddress){
+        this(customerName,1000,emailAddress);
+    }
+
+    public Customer(String customerName, double creditLimit, String emailAddress) {
+        this.customerName = customerName;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 }
+
